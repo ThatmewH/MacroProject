47d6a2ca672db2a1ad9796edd88c9a93d4b7fe74
ThatmewH <thatmewh@gmail.com>
2022-02-02 12:29:12 +1000

    Added CommandFunction which replaces MacroController.java as a non-static class that holds and runs Command.java. Added Listener class that runs CommandFunction.java on set events.

--- src/main/java/com/example/macroproject/Main.java
+++ src/main/java/com/example/macroproject/Main.java
@@ -8,9 +8,12 @@ public class Main {
     public static void main(String[] args) throws NativeHookException {
         GlobalScreen.registerNativeHook();
         Listener l = new Listener();
+
         GlobalScreen.addNativeMouseListener(l);
         GlobalScreen.addNativeKeyListener(l);
+
         StartApplication.main(args);
+
         GlobalScreen.removeNativeKeyListener(l);
         GlobalScreen.removeNativeMouseListener(l);
     }
